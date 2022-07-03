1c7958eb165bfb40ccc204e22b761fca10a8635a
21xx13 <alena2001.03.26@mail.ru>
2022-07-04 00:38:44 +0500

    Solved tasks

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using AutoMapper;
 using Game.Domain;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
 
@@ -9,21 +12,80 @@ namespace WebApi.Controllers
     [ApiController]
     public class UsersController : Controller
     {
-        // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-        public UsersController(IUserRepository userRepository)
+        private IUserRepository userRepository;
+        private IMapper mapper;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
         }
 
-        [HttpGet("{userId}")]
+        [HttpGet("{userId}", Name = nameof(GetUserById))]
+        [Produces("application/json", "application/xml")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var userFromRepo = userRepository.FindById(userId);
+            if (userFromRepo == null)
+                return NotFound();
+            var userDto = mapper.Map<UserDto>(userFromRepo);
+            return Ok(userDto);
         }
 
         [HttpPost]
-        public IActionResult CreateUser([FromBody] object user)
+        [Produces("application/json", "application/xml")]
+        public IActionResult CreateUser([FromBody] UserCreateDto user)
         {
-            throw new NotImplementedException();
+            if (user == null)
+                return BadRequest();
+            if (string.IsNullOrEmpty(user.Login) || !user.Login.All(c => char.IsLetterOrDigit(c)))
+                ModelState.AddModelError("Login", "Empty or not numbers");
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+            var userEntity = mapper.Map<UserEntity>(user);
+            var createdUserEntity = userRepository.Insert(userEntity);
+            return CreatedAtRoute(
+                nameof(GetUserById),
+                new { userId = createdUserEntity.Id }, 
+                createdUserEntity.Id);
+        }
+        
+        [HttpPut("{userId}")]
+        [Produces("application/json", "application/xml")]
+        public IActionResult UpdateUser([FromBody] UserUpdateDto user, [FromRoute] Guid userId)
+        {
+            if (user == null || userId == Guid.Empty)
+                return BadRequest();
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+            var newUserEntity = new UserEntity(userId);
+            mapper.Map(user, newUserEntity);
+            userRepository.UpdateOrInsert(newUserEntity, out var isInserted);
+            if (isInserted)
+                return CreatedAtRoute(
+                    nameof(GetUserById),
+                    new { userId = newUserEntity.Id }, 
+                    newUserEntity.Id);
+            return NoContent();
+        }
+        
+        [HttpPatch("{userId}")]
+        [Produces("application/json", "application/xml")]
+        public IActionResult PartiallyUpdateUser([FromBody] JsonPatchDocument<UserUpdateDto> patchDoc, [FromRoute] Guid userId)
+        {
+            if (patchDoc == null)
+                return BadRequest();
+            
+            
+            if (userId == Guid.Empty || userRepo == null)
+                return NotFound();
+            
+            
+            
+            var userRepo = userRepository.FindById(userId);
+            var updateDto = mapper.Map<UserUpdateDto>(userRepo);
+            patchDoc.ApplyTo(updateDto, ModelState);
+            TryValidateModel(updateDto);
+            return NoContent();
         }
     }
 }
